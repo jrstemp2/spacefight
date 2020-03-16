58351f9164bde384d0983291b3b241da4b6aeb88
John Stempowski <johnstempowski@quickenloans.com>
2020-03-16 12:27:29 -0400

    first attempt

---
+++ SpaceFight/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SpaceFight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MoveStars = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            this.LeftMove = new System.Windows.Forms.Timer(this.components);
+            this.RightMove = new System.Windows.Forms.Timer(this.components);
+            this.UpMove = new System.Windows.Forms.Timer(this.components);
+            this.DownMove = new System.Windows.Forms.Timer(this.components);
+            this.MoveLaser = new System.Windows.Forms.Timer(this.components);
+            this.MoveEnemiesTimer = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MoveStars
+            // 
+            this.MoveStars.Enabled = true;
+            this.MoveStars.Interval = 10;
+            this.MoveStars.Tick += new System.EventHandler(this.MoveStars_Tick);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.ErrorImage = null;
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(268, 399);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(50, 50);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            this.player.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // LeftMove
+            // 
+            this.LeftMove.Interval = 5;
+            this.LeftMove.Tick += new System.EventHandler(this.LeftMove_Tick);
+            // 
+            // RightMove
+            // 
+            this.RightMove.Interval = 5;
+            this.RightMove.Tick += new System.EventHandler(this.RightMove_Tick);
+            // 
+            // UpMove
+            // 
+            this.UpMove.Interval = 5;
+            this.UpMove.Tick += new System.EventHandler(this.UpMove_Tick);
+            // 
+            // DownMove
+            // 
+            this.DownMove.Interval = 5;
+            this.DownMove.Tick += new System.EventHandler(this.DownMove_Tick);
+            // 
+            // MoveLaser
+            // 
+            this.MoveLaser.Interval = 20;
+            this.MoveLaser.Tick += new System.EventHandler(this.MoveLaser_Tick);
+            // 
+            // MoveEnemiesTimer
+            // 
+            this.MoveEnemiesTimer.Enabled = true;
+            this.MoveEnemiesTimer.Tick += new System.EventHandler(this.EnemyMove_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.player);
+            this.ForeColor = System.Drawing.SystemColors.Window;
+            this.Name = "Form1";
+            this.Text = "Rockets";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer MoveStars;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer LeftMove;
+        private System.Windows.Forms.Timer RightMove;
+        private System.Windows.Forms.Timer UpMove;
+        private System.Windows.Forms.Timer DownMove;
+        private System.Windows.Forms.Timer MoveLaser;
+        private System.Windows.Forms.Timer MoveEnemiesTimer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SpaceFight/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceFight
+{
+    public partial class Form1 : Form
+    {
+        bool canFire;
+        PictureBox[] stars;
+        int backgroundSpeed;
+        int playerSpeed;
+        Random rand;
+
+        //lasers
+        PictureBox[] lasers;
+        int laserSpeed;
+
+        //enemies
+        PictureBox[] enemies;
+        int enemySpeed;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundSpeed = 4;
+            playerSpeed = 4;
+            enemySpeed = 4;
+            stars = new PictureBox[30];
+            rand = new Random();
+
+            laserSpeed = 4;
+            lasers = new PictureBox[1];
+            
+            canFire = true;
+
+            
+
+            Image laser = Image.FromFile(@"assets\laser.png");
+            Image enemy1 = Image.FromFile(@"assets\en1.png");
+            Image enemy2 = Image.FromFile(@"assets\en2.png");
+            Image enemy3 = Image.FromFile(@"assets\en3.png");
+
+            enemies = new PictureBox[10];
+
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i] = new PictureBox();
+                enemies[i].Size = new Size(40, 40);
+                enemies[i].SizeMode = PictureBoxSizeMode.Zoom;
+                enemies[i].BorderStyle = BorderStyle.None;
+                enemies[i].Visible = false;
+                this.Controls.Add(enemies[i]);
+                enemies[i].Location = new Point((i + 1) * 50, -50);
+
+            }
+
+            enemies[0].Image = enemy1;
+            enemies[1].Image = enemy1;
+            enemies[2].Image = enemy2;
+            enemies[3].Image = enemy3;
+            enemies[4].Image = enemy1;
+            enemies[5].Image = enemy3;
+            enemies[6].Image = enemy2;
+            enemies[7].Image = enemy1;
+            enemies[8].Image = enemy2;
+            enemies[9].Image = enemy3;
+
+
+
+            //Create Laser
+            for (int i = 0; i < lasers.Length; i++)
+            {
+                lasers[i] = new PictureBox();
+                lasers[i].Size = new Size(5, 10);
+                lasers[i].Image = laser;
+                lasers[i].SizeMode = PictureBoxSizeMode.Zoom;
+                lasers[i].BorderStyle = BorderStyle.None;
+                this.Controls.Add(lasers[i]);
+            }
+
+
+                for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new PictureBox();
+                stars[i].BorderStyle = BorderStyle.None;
+                stars[i].Location = new Point(rand.Next(20, 580), rand.Next(-10, 400));
+                if (i % 2 == 1)
+                {
+                    stars[i].Size = new Size(2, 2);
+                    stars[i].BackColor = Color.Red;
+                }
+                else if (i % 2 == 0)
+                {
+                    stars[i].Size = new Size(3, 3);
+                    stars[i].BackColor = Color.Blue;
+                }
+                this.Controls.Add(stars[i]);
+            }
+        }
+
+        private void MoveStars_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i].Top += backgroundSpeed;
+
+                if(stars[i].Top >= this.Height)
+                {
+                    stars[i].Top = -stars[i].Height;
+                }
+            }
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DownMove_Tick(object sender, EventArgs e)
+        {
+            if (player.Top < 400)
+            {
+                player.Top += playerSpeed;
+            }
+        }
+
+        private void UpMove_Tick(object sender, EventArgs e)
+        {
+            if (player.Top > 10)
+            {
+                player.Top -= playerSpeed;
+            }
+        }
+
+        private void RightMove_Tick(object sender, EventArgs e)
+        {
+            if (player.Right < 580)
+            {
+                player.Left += playerSpeed;
+            }
+        }
+
+        private void LeftMove_Tick(object sender, EventArgs e)
+        {
+            if (player.Left > 10)
+            {
+                player.Left -= playerSpeed;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                RightMove.Start();
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                LeftMove.Start();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                UpMove.Start();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                DownMove.Start();
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                
+                
+                if (canFire == true)
+                {
+                    lasers[0].Location = new Point(player.Location.X + 20, player.Location.Y - 0);
+                    MoveLaser.Start();
+                }
+                
+                
+                
+            }
+        }
+         
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            RightMove.Stop();
+            LeftMove.Stop();
+            UpMove.Stop();
+            DownMove.Stop();
+           // canFire = false;
+        }
+
+        private void MoveLaser_Tick(object sender, EventArgs e)
+        {
+            
+
+            
+
+            for (int i = 0; i < lasers.Length; i++)
+            {
+                if (lasers[i].Top > 0)
+                {
+                    canFire = false;
+                    lasers[i].Visible = true;
+                    lasers[i].Top -= laserSpeed;
+         
+                }
+                else
+                {
+                    canFire = true;
+                    lasers[i].Visible = false;
+                    MoveLaser.Stop();
+                }
+                
+            }
+            
+
+            
+        }
+
+        private void EnemyMove_Tick(object sender, EventArgs e)
+        {
+            MoveEnemies(enemies, enemySpeed);
+        }
+        private void MoveEnemies(PictureBox[] array, int speed)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Visible = true;
+                array[i].Top += speed;
+
+                if (array[i].Top > this.Height)
+                {
+                    array[i].Location = new Point((i + 1) * 50, -200);
+                }
+            }
+        }
+    }
+}
